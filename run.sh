7c5be26e1d66d0c5031f764705ecf763280badbc
Thomas Bonnici <thomas.bonnici@icloud.com>
2024-03-30 16:58:19 +0100

    v0.0.1

--- run.sh
+++ run.sh
@@ -13,12 +13,35 @@ check_ffmpeg() {
 # Function to install ffmpeg
 install_ffmpeg() {
     echo "Installing ffmpeg..."
+    # This is an example for a Debian-based system:
     sudo apt-get update
     sudo apt-get install -y ffmpeg
 }
 
+# Function to check if the .venv directory exists and install dependencies
+setup_virtualenv() {
+    if [ ! -d ".venv" ]; then
+        echo "Virtual environment not found. Setting up the environment..."
+        pdm install
+    else
+        echo "Virtual environment found."
+    fi
+}
+
+# Function to activate virtual environment
+activate_virtualenv() {
+    echo "Activating the virtual environment..."
+    source .venv/bin/activate
+}
+
 # Check if ffmpeg is installed
 check_ffmpeg
+
+# Check if the virtual environment exists and set it up if it doesn't
+setup_virtualenv
+
+# Activate virtual environment
+activate_virtualenv
 
 # Run uvicorn server
 echo "Starting uvicorn server..."
